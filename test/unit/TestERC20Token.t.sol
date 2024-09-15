d0b7efe22bdeec7385fcc0575156912505731946
spencerj411 <spencer.j411@gmail.com>
2024-09-15 17:56:36 +1000

    wrote a unit test to confirm delegating transfer power works

--- test/unit/TestERC20Token.t.sol
+++ test/unit/TestERC20Token.t.sol
@@ -9,7 +9,7 @@ import {DeployERC20Token} from "script/DeployERC20Token.s.sol";
 contract TestERC20Token is Test {
     ERC20Token erc20Token;
 
-    uint256 constant STARTING_BALANCE = 10 ether;
+    uint256 constant STARTING_BALANCE = 100 ether;
     address user1 = makeAddr("1");
     address user2 = makeAddr("2");
 
@@ -21,5 +21,29 @@ contract TestERC20Token is Test {
 
     function testUser1HasBalance() public view {
         assertEq(erc20Token.balanceOf(user1), STARTING_BALANCE);
+    }
+
+    function testTransferPowerDelegationWorks() public {
+        // Arrange
+        uint256 balanceOfUser1 = erc20Token.balanceOf(user1);
+        vm.prank(user1);
+        bool user2IsApprovedAsSpender = erc20Token.approve(
+            user2,
+            balanceOfUser1
+        );
+        // Act
+        uint256 tokensTransferredViaDelegatedSpender = 1 ether;
+        vm.prank(user2);
+        erc20Token.transferFrom(
+            user1,
+            user2,
+            tokensTransferredViaDelegatedSpender
+        );
+        // Assert
+        assert(user2IsApprovedAsSpender);
+        assertEq(
+            erc20Token.balanceOf(user1),
+            STARTING_BALANCE - tokensTransferredViaDelegatedSpender
+        );
     }
 }
