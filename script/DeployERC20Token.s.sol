c6538e5ca13f47c8c1f7364394b606cb391b4740
spencerj411 <spencer.j411@gmail.com>
2024-09-15 11:32:16 +1000

    added deploy script and empy test file

---
+++ script/DeployERC20Token.s.sol
@@ -0,0 +1,22 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.19;
+
+import {Script, console} from "forge-std/Script.sol";
+import {ERC20Token} from "src/ERC20Token.sol";
+
+uint256 constant TOTAL_SUPPLY = 1000 ether;
+string constant TOKEN_NAME = "My Token";
+string constant TOKEN_TICKER_SYMBOL = "MTK";
+
+contract DeployERC20Token is Script {
+    function run() external {
+        vm.startBroadcast();
+        ERC20Token erc20Token = new ERC20Token(
+            TOTAL_SUPPLY,
+            TOKEN_NAME,
+            TOKEN_TICKER_SYMBOL
+        );
+        vm.stopBroadcast();
+    }
+}
