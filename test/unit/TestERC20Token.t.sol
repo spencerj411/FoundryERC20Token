4e1608cc47f87665b9420ce570cd9a222d00df9d
spencerj411 <spencer.j411@gmail.com>
2024-09-15 16:53:19 +1000

    created first test; owner of erc20token contract transfers to another contract

---
+++ test/unit/TestERC20Token.t.sol
@@ -0,0 +1,25 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.19;
+
+import {Test, console} from "forge-std/Test.sol";
+import {ERC20Token} from "src/ERC20Token.sol";
+import {DeployERC20Token} from "script/DeployERC20Token.s.sol";
+
+contract TestERC20Token is Test {
+    ERC20Token erc20Token;
+
+    uint256 constant STARTING_BALANCE = 10 ether;
+    address user1 = makeAddr("1");
+    address user2 = makeAddr("2");
+
+    function setUp() public {
+        erc20Token = (new DeployERC20Token()).run();
+        vm.prank(msg.sender); // which is also msg.sender of erc20Token; owns the total supply when erc20Token is initiated
+        erc20Token.transfer(user1, STARTING_BALANCE);
+    }
+
+    function testUser1HasBalance() public view {
+        assertEq(erc20Token.balanceOf(user1), STARTING_BALANCE);
+    }
+}
